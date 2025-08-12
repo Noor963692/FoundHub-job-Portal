2737fc7e52f5282ac7a9e3a3f30570009a50055d
Noor Mohammad <nm856565@gmail.com>
2025-08-12 22:47:01 +0530

    url setup

--- back-end/index.js
+++ back-end/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin: 'https://found-hub-job-portal.vercel.app/',
+    origin: 'https://found-hub-job-portal.vercel.app',
     credentials:true
 }
 app.use(cors(corsOptions));
